5823a452b2d066f18afcc9611d749b77b563be10
erikcom <erikwong@chaatz.com>
2017-02-05 20:14:36 +0800

    Update AMP standard

--- src/MaybeT.hs
+++ src/MaybeT.hs
@@ -1,5 +1,7 @@
 module MaybeT where
 
+import Control.Monad (liftM, ap)
+
 newtype MaybeT m a = MaybeT {
     runMaybeT :: m (Maybe a)
 }
@@ -7,34 +9,31 @@ newtype MaybeT m a = MaybeT {
 returnMT :: (Monad m) => a -> MaybeT m a
 returnMT a = MaybeT $ return (Just a)
 
---failMT :: (Monad m) => t -> MaybeT m a
---failMT _ = MaybeT $ return Nothing
+failMT :: (Monad m) => t -> MaybeT m a
+failMT _ = MaybeT $ return Nothing
 
---altBindMT :: (Monad m) => MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
---altBindMT x f = MaybeT $ runMaybeT x >>= maybe (return Nothing) (runMaybeT . f)
+{- maybe :: b -> (a -> b) -> Maybe a -> b
+, 1 `maybe` (+1) (Just 1) == 2
+, 1 `maybe` (+1) Nothing == 1
+-}
+altBindMT :: (Monad m) => MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
+altBindMT x f = MaybeT $ runMaybeT x >>= maybe (return Nothing) (runMaybeT . f)
 
---bindMT :: (Monad m) => MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
---bindMT x f = MaybeT $ do
---    unwrapped <- runMaybeT x
---    case unwrapped of
---        Nothing -> return Nothing
---        Just y -> runMaybeT (f y)
-
---fmapMT :: (a -> b) -> MaybeT m a -> MaybeT m b
---apMT :: MaybeT m (a -> b) -> MaybeT m a -> MaybeT m b
+bindMT :: (Monad m) => MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
+bindMT x f = MaybeT $ do
+    unwrapped <- runMaybeT x
+    case unwrapped of
+        Nothing -> return Nothing
+        Just y -> runMaybeT (f y)
 
 instance (Monad m) => Functor (MaybeT m) where
-    fmap = undefined
+    fmap = liftM
 
 instance (Monad m) => Applicative (MaybeT m) where
     pure = returnMT
-    (<*>) = undefined
+    (<*>) = ap
 
 instance (Monad m) => Monad (MaybeT m) where
-    {- maybe :: b -> (a -> b) -> Maybe a -> b
-    , 1 `maybe` (+1) (Just 1) == 2
-    , 1 `maybe` (+1) Nothing == 1
-    -}
     return = returnMT
-    fail _ = MaybeT $ return Nothing
-    (>>=) x f = MaybeT $ runMaybeT x >>= (return Nothing) `maybe` (runMaybeT . f)
+    fail = failMT
+    (>>=) = bindMT
